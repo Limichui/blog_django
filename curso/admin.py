d361418c59318a9d232c3d5154f21eaca2b337d6
Limichui <limbert.olmos.m@gmail.com>
2024-12-05 22:57:47 -0400

    Personalizar la vista de los modelos Categoria, Publicacion y Comentario en el administrador

--- curso/admin.py
+++ curso/admin.py
@@ -1,11 +1,33 @@
 from django.contrib import admin
 from .models import Categoria, Publicacion, Comentario
 
-admin.site.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'fecha_registro')
+    ordering = ('nombre',)
+    search_fields = ('nombre',)
+    
+    def fecha_registro(self, obj): 
+        return obj.created 
+    fecha_registro.short_description = 'Fecha Registro'
+    
+admin.site.register(Categoria, CategoriaAdmin)
 
 class PublicacionAdmin(admin.ModelAdmin):
-    list_display = ('titulo', 'categoria', 'profesor')
+    list_display = ('titulo', 'duracion', 'estado', 'categoria', 'profesor')
+    ordering = ('titulo',)
+    search_fields = ('titulo',)
+    list_filter = ('estado',)
 
-admin.site.register(Publicacion)
+admin.site.register(Publicacion, PublicacionAdmin)
 
-admin.site.register(Comentario)
+class ComentarioAdmin(admin.ModelAdmin):
+    list_display = ('contenido', 'autor', 'estado', 'publicacion', 'fecha_registro')
+    ordering = ('-created',) # Orden descendente por el campo 'created' es con '-created'
+    search_fields = ('contenido',)
+    list_filter = ('estado',)
+    
+    def fecha_registro(self, obj): 
+        return obj.created 
+    fecha_registro.short_description = 'Fecha Registro'
+    
+admin.site.register(Comentario, ComentarioAdmin)
